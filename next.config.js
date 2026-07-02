d176d4501ff363bccf3733e50976ff0ee84ffaf9
Md. Iqbal Haider Khan <ihkokil@gmail.com>
2026-07-02 22:13:18 +0600

    chore: completely remove PWA implementation and dependencies

--- next.config.js
+++ next.config.js
@@ -1,10 +1,4 @@
-import withSerwistInit from "@serwist/next";
 
-const withSerwist = withSerwistInit({
-    swSrc: "src/app/sw.ts",
-    swDest: "public/sw.js",
-    disable: process.env.NODE_ENV === "development",
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -43,4 +37,4 @@ const nextConfig = {
     },
 };
 
-export default withSerwist(nextConfig);
+export default nextConfig;
